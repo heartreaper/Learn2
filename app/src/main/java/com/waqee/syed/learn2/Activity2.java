eace41ac76b77b46c5ebb63e92cdc8eecbbf70a6
heartreaper <waqee15@gmail.com>
2016-01-30 13:35:33 +0300

    1 asdfasdfasdf

--- app/src/main/java/com/waqee/syed/learn2/Activity2.java
+++ app/src/main/java/com/waqee/syed/learn2/Activity2.java
@@ -1,5 +1,7 @@
 package com.waqee.syed.learn2;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,5 +14,17 @@ public class Activity2 extends AppCompatActivity {
         setContentView(R.layout.activity_learn2_2);
         Log.d("Syed", "Inside Activity 2");
         Log.d("Syed", "Activity 2 onCreate");
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation==Configuration.ORIENTATION_PORTRAIT){
+            Intent i = new Intent(this, Learn2.class);
+            startActivity(i);
+            Log.d("Syed", "Portrait + Activity 2 Opened");
+        }
+        else if (newConfig.orientation==Configuration.ORIENTATION_LANDSCAPE){
+            Log.d("Syed", "Landscape");
+        }
     }
 }
